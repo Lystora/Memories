e46b50e7326319ab69d79fce88fa6f309744b272
Allan Booz <playgame2@hotmail.fr>
2019-11-21 20:11:13 +0400

    commentaire splashscreen

--- app/src/main/java/com/example/memories/SplashScreen.java
+++ app/src/main/java/com/example/memories/SplashScreen.java
@@ -11,22 +11,25 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 3000;
 
-    private ImageView imgLogo;
+    private static int SPLASH_TIME_OUT = 3000;
+    protected ImageView Logo;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Hide app title bar.
+        // Cache la barre de titre.
         getSupportActionBar().hide();
 
-        // Make app full screen to hide top status bar.
+        // L'application est en fullscreen.
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        // Le logo
-        imgLogo = (ImageView) findViewById(R.id.imgLogo);
+        // Initialisation de l'ImageView (le logo)
+        Logo = (ImageView) findViewById(R.id.imgLogo);
+
+        // Splash Screen pendant 3 secondes puis on passe sur l'activity Camera
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
